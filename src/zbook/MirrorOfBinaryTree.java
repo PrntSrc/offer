38d151b23ac14bfd81276661fadec8230d66bb81
ylins <yue.lin@outlook.com>
2018-03-09 21:45:59 +0800

    initial commit

---
+++ src/zbook/MirrorOfBinaryTree.java
@@ -0,0 +1,29 @@
+package zbook;
+
+/**
+ * @author Yue
+ * @since 2018/3/9
+ */
+public class MirrorOfBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode Mirror(TreeNode root) {
+        if (root == null) return null;
+        if (root.left == null && root.right == null) return root;
+        TreeNode node = root.left;
+        root.left = root.right;
+        root.right = node;
+        Mirror(root.left);
+        Mirror(root.right);
+        return root;
+    }
+
+}
