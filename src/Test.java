aafcfb692dbbcb8b968641666f150d8e4c0748b2
ylins <yue.lin@outlook.com>
2018-04-02 18:44:51 +0800

    initial commit

--- src/Test.java
+++ src/Test.java
@@ -8,15 +8,27 @@ import java.util.Scanner;
  */
 public class Test {
 
-    int count;
-    public int count() {
-        count++;
-        return count;
+//    int count;
+//    public int count() {
+//        count++;
+//        return count;
+//    }
+    static int foo() {
+        int i = 10;
+        try {
+            i = --i / 0;
+            return i--;
+        } catch (Exception e) {
+            i = i-- / 0;
+            return --i;
+        } finally {
+            --i;
+            return i--;
+        }
     }
     public static void main(String[] args) {
-        Test test = new Test();
-        test.count();
-        System.out.println(test.count());
+        Map<> map = new HashMap()
+        System.out.println(foo());
 //        System.out.println(new Test().count);
 //        new Test().count;
     }
