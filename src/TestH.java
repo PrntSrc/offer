5759f39215a9d5d6eb52a098ace31611b049c562
ylins <yue.lin@outlook.com>
2018-05-09 23:14:09 +0800

    initial commit

--- src/TestH.java
+++ src/TestH.java
@@ -4,9 +4,36 @@
  */
 public class TestH {
 
+    public TestH() {
+        this(10);
+    }
+
+    public TestH(int data) {
+        this.data = data;
+    }
+
+    public void display() {
+//        class Decrementer {
+//            public void decrement(){data--;}};
+//        Decrementer d = new Decrementer();
+//        d.decrement();
+        System.out.println("data=" + data);
+    }
+    private int data;
+
+            class Decrementer {
+            public void decrement(double data){data = data - 1.0;}}
+
     public static void main(String[] args) {
-        String s1 = "3";
-        String s2 = "30";
-        System.out.println(s2.compareTo(s1));
+                Integer a = new Integer("32111");
+    }
+    static char[] getCharArray(byte[] arr) {
+                char[] carr = new char[4];
+                int i = 0;
+                for (byte c : arr) {
+                    carr[i] = (char)c++;
+                    i++;
+                }
+                return carr;
     }
 }
