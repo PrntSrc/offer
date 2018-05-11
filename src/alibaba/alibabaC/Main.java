b8b669c87519b8547ab91ed086fb6b3dc944bdc0
ylins <yue.lin@outlook.com>
2018-05-11 18:22:41 +0800

    initial commit

---
+++ src/alibaba/alibabaC/Main.java
@@ -0,0 +1,14 @@
+package alibaba.alibabaC;
+
+import java.util.Scanner;
+
+/**
+ * @author Yue
+ * @since 2018/5/11
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
